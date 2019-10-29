6d9bc26fd80ca51a796ab96799924bbc1c8bdb41
rasimon <rasimon22@gmail.com>
2019-10-28 21:44:58 -0500

    report progress and removed duplicates

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <stdlib.h>
 #include <vector>
+#include <memory>
 void test_with_uniform(int c, int s, int k, std::string dist) {
 
     {
@@ -14,28 +15,38 @@ void test_with_uniform(int c, int s, int k, std::string dist) {
     DataGen<size_t, Distribution::Uniform> gen(1,c - 1);
 
     AdjacencyList a1(c);
+    std::unique_ptr<int[]> duplicates(new int[c]);
+    for(int i = 0; i < c; ++i){
+        duplicates[i] = c*2;
+    }
     for (int i = 0; i < s; ++ i) {
         //for each student
         std::vector<size_t> classes;
         for (int j = 0; j < k; ++j) {
             //for classes per student
             //pick a random class
-            classes.push_back(gen.yield());
+            int rand = gen.yield();
+            //guess again if duplicate
+            while(duplicates[rand] == i){
+                rand = gen.yield();
+            }
+            classes.push_back(rand);
+            duplicates[rand] = i;
         }
 
-        for (int k = 0; k < classes.size(); ++k) {
-            //iterate over each class per student
-            for (int l = k; l < classes.size(); ++l) {
-                //make edge for all other classes in students
-                //schedule
-                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
-            }
-        }
+//        for (int k = 0; k < classes.size(); ++k) {
+//            //iterate over each class per student
+//            for (int l = k; l < classes.size(); ++l) {
+//                //make edge for all other classes in students
+//                //schedule
+//                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
+//            }
+//        }
     }
     a1.print();
   }
 
-std::cout << Chron::Timer::duration(std::string("uniform_timer"), Chron::Scale::Nanoseconds) \
+std::cout << Chron::Timer::duration(std::string("uniform_timer"), Chron::Scale::Milliseconds) \
     << "ns" << std::endl;
 }
 void test_with_normal(int c, int s, int k, std::string dist) {
@@ -44,7 +55,10 @@ void test_with_normal(int c, int s, int k, std::string dist) {
 
     Chron::Timer main_t(std::string("normal_timer"));
     DataGen<size_t, Distribution::Uniform> gen(1,c);
-
+    std::unique_ptr<int[]> duplicates(new int[c]);
+    for(int i = 0; i < c; ++i){
+        duplicates[i] = c*2;
+    }
     AdjacencyList a1(c);
     for (int i = 0; i < s; ++ i) {
         //for each student
@@ -52,17 +66,22 @@ void test_with_normal(int c, int s, int k, std::string dist) {
         for (int j = 0; j < k; ++j) {
             //for classes per student
             //pick a random class
-            classes.push_back(rand_normal(c/2, c/4));
+            int rand = rand_normal(c/2, c/4);
+            while (duplicates[rand] == i) {
+                rand = rand_normal(c/2, c/4);
+            }
+            classes.push_back(rand);
+            duplicates[rand] = i;
         }
 
-        for (int k = 0; k < classes.size(); ++k) {
-            //iterate over each class per student
-            for (int l = k; l < classes.size(); ++l) {
-                //make edge for all other classes in students
-                //schedule
-                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
-            }
-        }
+//        for (int k = 0; k < classes.size(); ++k) {
+//            //iterate over each class per student
+//            for (int l = k; l < classes.size(); ++l) {
+//                //make edge for all other classes in students
+//                //schedule
+//                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
+//            }
+//        }
     }
     a1.print();
   }
@@ -76,6 +95,10 @@ void test_with_linear(int c, int s, int k, std::string dist) {
     Chron::Timer main_t(std::string("linear_timer"));
     DataGen<size_t, Distribution::Uniform> gen(1,c-1);
     DataGen<size_t, Distribution::Uniform> gen2(1,c-1);
+    std::unique_ptr<int[]> duplicates(new int[c]);
+    for(int i = 0; i < c; ++i) {
+        duplicates[i] = c*2;
+    }
 
     AdjacencyList a1(c);
     for (int i = 0; i < s; ++ i) {
@@ -84,17 +107,22 @@ void test_with_linear(int c, int s, int k, std::string dist) {
         for (int j = 0; j < k; ++j) {
             //for classes per student
             //pick a random class
-            classes.push_back(DataGen<size_t, Distribution::Uniform>::rand_linear(gen,gen2));
+            int rand = DataGen<size_t, Distribution::Uniform>::rand_linear(gen,gen2);
+            while(duplicates[rand] == i) {
+                rand = DataGen<size_t, Distribution::Uniform>::rand_linear(gen,gen2);
+            }
+            classes.push_back(rand);
+            duplicates[rand] = i;
         }
 
-        for (int k = 0; k < classes.size(); ++k) {
-            //iterate over each class per student
-            for (int l = k; l < classes.size(); ++l) {
-                //make edge for all other classes in students
-                //schedule
-                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
-            }
-        }
+//        for (int k = 0; k < classes.size(); ++k) {
+//            //iterate over each class per student
+//            for (int l = k; l < classes.size(); ++l) {
+//                //make edge for all other classes in students
+//                //schedule
+//                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
+//            }
+//        }
     }
     a1.print();
   }
@@ -108,6 +136,10 @@ void test_with_tiered(int c, int s, int k, std::string dist) {
     DataGen<int, Distribution::Uniform> gen2((int)c/4, (int)c/2);
     DataGen<int, Distribution::Uniform> gen3((int)c/2,(int)(3*c/4));
     DataGen<int, Distribution::Uniform> gen4((int)(3*c/4),c);
+    std::unique_ptr<int[]> duplicates(new int[c]);
+    for(int i = 0; i < c; ++i) {
+        duplicates[i] = c*2;
+    }
     int probs[4] = {10, 11, 13, 50 };
     AdjacencyList a1(c);
     for (int i = 0; i < s; ++ i) {
@@ -116,17 +148,22 @@ void test_with_tiered(int c, int s, int k, std::string dist) {
         for (int j = 0; j < k; ++j) {
             //for classes per student
             //pick a random class
-            classes.push_back(rand_tiered(gen, gen2, gen3, gen4, probs));
+            int rand = rand_tiered(gen, gen2, gen3, gen4, probs);
+            while(duplicates[rand] == i) {
+                rand =  rand_tiered(gen, gen2, gen3, gen4, probs);
+            }
+            classes.push_back(rand);
+            duplicates[rand] = i;
         }
 
-        for (int k = 0; k < classes.size(); ++k) {
-            //iterate over each class per student
-            for (int l = k; l < classes.size(); ++l) {
-                //make edge for all other classes in students
-                //schedule
-                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
-            }
-        }
+//        for (int k = 0; k < classes.size(); ++k) {
+//            //iterate over each class per student
+//            for (int l = k; l < classes.size(); ++l) {
+//                //make edge for all other classes in students
+//                //schedule
+//                if(classes[k] != classes[l]) a1.insert(classes[k], classes[l]);
+//            }
+//        }
     }
     a1.print();
   }
